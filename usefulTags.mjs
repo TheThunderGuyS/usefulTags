5b3c2ff1b30d46bb1b06bbc2b46d79f0b7c23a5c
TheThunderGuyS <saihaj.sran@gmail.com>
2020-11-24 17:49:52 -0500

    Some fixes - Fixes #7 - stripIndent will remove trailing spaces on the first line - Updated comments

--- usefulTags.mjs
+++ usefulTags.mjs
@@ -1,5 +1,5 @@
 "use strict";
-//Manage types (return string, add together array)
+//Return a string from arrays, throw on non-string/array
 function manageTypes(templateString, ...literals) {
     if (typeof templateString === "string") return templateString;
     else if (Array.isArray(templateString)) {
@@ -19,11 +19,12 @@ export function stripIndent(templateString, ...literals) {
     const string = manageTypes(templateString, ...literals);
 
     const indents = string
-        .match(/^\s*/gm) //Find whitespace at the beginning of lines
-        .reduce((r, a) => Math.min(r, a.length), Infinity); //Get the number of whitespace characters from this
+        .match(/^[ \t]*?(?=\S)/gm) //Find whitespace at the beginning of lines
+        .reduce((a, c) => Math.min(a, c.length), Infinity); //Get the number of whitespace characters
 
     return string
-        .replace(new RegExp(`^[ \t]{${indents}}`, "gm"), "") //Trim the spaces
+        .replace(new RegExp(`^[ \t]{${indents}}`, "gm"), "") //Trim the whitespace
+        .replace(/^[ \t]+/g, "") //Trim the trailing first-line whitespace, if any
         .replace(/^\n/, ""); //Trim the first newline
 }
 
@@ -40,7 +41,7 @@ export function oneLine(templateString, ...literals) {
 
     return string
         .replace(/^\s+/gm, " ") //Trim all excess whitespace, replacing it with one space
-        .replace(/^\s/g, "") //Remove the beginning space
+        .replace(/^\s/g, "") //Trim the beginning space
         .replace(/\n/g, ""); //Trim all newlines
 }
 
